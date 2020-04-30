95d715da097a3b19af2dce2582033cfee61ebeaa
Iheycuff Norambuena <ihncode@gmail.com>
2020-04-29 23:33:56 -0400

    Agregamos Bienvenida y botón de Inicio de juego

--- js/iniciar.js
+++ js/iniciar.js
@@ -1,10 +1,15 @@
-movimientos = 0;
-repartirTarjetas();
+function iniciar () {
+  movimientos = 0;
+  repartirTarjetas();
 
-document.querySelectorAll(".tarjeta").forEach(function(e) {
-  e.addEventListener("click", descubrir);
-  // e.addEventListener("click", tarjetasDescubiertas);
-});
+  document.querySelectorAll(".tarjeta").forEach(function(e) {
+    e.addEventListener("click", descubrir);
+    // e.addEventListener("click", tarjetasDescubiertas);
+  });
 
-iniciarCronometro();
-// iniciarTemporizador();
+  iniciarCronometro();
+  // iniciarTemporizador();
+}
+
+var botonIniciarJuego = document.querySelector("#iniciarJuego");
+botonIniciarJuego.addEventListener ("click", iniciar);
