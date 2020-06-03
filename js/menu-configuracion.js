ca752b9afde12e655d7444e964a858cfb3bcf3fd
Iheycuff Norambuena <ihncode@gmail.com>
2020-06-02 21:40:35 -0400

    Solución de bug del menu de configuración en Mobile. El problema es por usar vh, se solucionó usando JS para calcular el vh

--- js/menu-configuracion.js
+++ js/menu-configuracion.js
@@ -1,3 +1,11 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 function activarPistas() {
   if (estadoPista === "activo") {
     estadoPista = "inactivo";
