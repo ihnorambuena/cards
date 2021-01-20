a675fabca93583652d7a1d9e64c4f05f65548ca5
Iheycuff Norambuena <ihncode@gmail.com>
2021-01-20 11:02:11 -0300

    Mejorando vh unit en JS

--- js/menu-configuracion.js
+++ js/menu-configuracion.js
@@ -1,9 +1,11 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.querySelector(".vh").innerText="vh es: " + vh + "px";
 
 window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.querySelector(".vh").innerText="vh es: " + vh + "px";
 });
 
 function activarPistas() {
